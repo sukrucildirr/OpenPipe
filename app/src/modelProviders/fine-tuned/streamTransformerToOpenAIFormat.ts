0f02b586de34383bf930030fbe18a38ae4c02ead
sukrucildirr <sukrucildirr@gmail.com>
2025-07-21 20:43:05 +0300

    Update streamTransformerToOpenAIFormat.ts

--- app/src/modelProviders/fine-tuned/streamTransformerToOpenAIFormat.ts
+++ app/src/modelProviders/fine-tuned/streamTransformerToOpenAIFormat.ts
@@ -8,7 +8,7 @@ const FUNCTION_ARGS_TAG = "<arguments>";
 export type ChatCompletionChunkWithUsage = ChatCompletionChunk & { usage?: CompletionUsage };
 
 /**
- * This function checks the stream, defines if it is a regular text response or tool call, and returns the correct OpenAI compatable stream.
+ * This function checks the stream, defines if it is a regular text response or tool call, and returns the correct OpenAI compatible stream.
  * We need this custom logic because Fine-tuned models return tool calls as a regular text completion.
  * In this case we need to deserialize the content transforming "<function>func_name<arguments>args" into a tool_calls object.
  */
